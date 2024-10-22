d74e1d8caa5001c8fe465b938a2ae0f8b85c7a7c
KIRILLQST <146109722+KIRILLQST@users.noreply.github.com>
2024-10-22 18:19:49 +0300

    Add files via upload

---
+++ 1 задание.cs
@@ -0,0 +1,21 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите числа через пробел:");
+        string input = Console.ReadLine();
+        int[] numbers = input.Split(' ')
+                             .Select(int.Parse).ToArray();
+
+        int[] result = MoveZerosToEnd(numbers);
+        Console.WriteLine("Массив после перемещения нулей: " + string.Join(", ", result));
+    }
+    static int[] MoveZerosToEnd(int[] arr)
+    {
+        int[] nonZero = arr.Where(num => num != 0).ToArray();
+        int zeroCount = arr.Length - nonZero.Length;
+        int[] result = new int[arr.Length];
+        Array.Copy(nonZero, result, nonZero.Length); return result;
+    }
+}
