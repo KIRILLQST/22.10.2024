3a9e5677dbff524c851f508fd86b1bd321551cac
KIRILLQST <146109722+KIRILLQST@users.noreply.github.com>
2024-10-24 18:39:18 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Kata
+{
+    public class CountUniqueElements
+    {
+        public static int CountUniqueElementsInList(List<int> numbers)
+        {
+
+            HashSet<int> uniqueElements = new HashSet<int>();
+
+            
+            foreach (var number in numbers)
+            {
+                uniqueElements.Add(number);
+            }
+
+            return uniqueElements.Count;
+        }
+    }
+}
---
+++ 4.cs
@@ -0,0 +1,40 @@
+﻿
+public class ParenthesesChecker
+{
+    public bool IsBalanced(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (stack.Count == 0)
+                {
+                    return false; 
+                }
+
+                char last = stack.Pop();
+                if (!IsPair(last, c))
+                {
+                    return false; 
+                }
+            }
+        }
+
+        return stack.Count == 0; 
+    }
+
+    private bool IsPair(char open, char close)
+    {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+}
+
+
